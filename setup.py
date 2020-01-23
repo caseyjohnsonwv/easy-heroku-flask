a6bf0d7c0c63ef2c030c5a915e1b80d8f7ecc647
caseyjohnsonwv <caseyjohnsonwv@gmail.com>
2020-01-23 12:06:16 -0500

    installing package only installs /bin script

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name = "easy-heroku-flask",
-  version = "0.0.1",
+  version = "0.0.2",
   author = "Casey Johnson",
   author_email = "caseyjohnsonwv@gmail.com",
   description = "Create new Heroku/Flask file structures in one command.",
   long_description = long_description,
   long_description_content_type = "text/markdown",
   url = "https://github.com/caseyjohnsonwv/easy-heroku-flask",
-  packages = setuptools.find_packages(),
+  packages = [],
   include_package_data = True,
   scripts = ["bin/easy-heroku-flask"],
   install_requires = [],
