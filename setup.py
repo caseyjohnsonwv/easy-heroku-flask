dc84a98e53cbb943d2080e44f82e058225361a3e
caseyjohnsonwv <caseyjohnsonwv@gmail.com>
2020-01-23 21:10:33 -0500

    removed readme render from setup

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
-
 setuptools.setup(
   name = "easy-heroku-flask",
   version = "0.0.2",
   author = "Casey Johnson",
   author_email = "caseyjohnsonwv@gmail.com",
   description = "Create new Heroku/Flask file structures in one command.",
-  long_description = long_description,
+  long_description = "https://gitub.com/caseyjohnsonwv/easy-heroku-flask",
   long_description_content_type = "text/markdown",
   url = "https://github.com/caseyjohnsonwv/easy-heroku-flask",
   packages = [],
