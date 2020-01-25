806797677e0a2a4f15a747da370b14a23196e1b7
caseyjohnsonwv <caseyjohnsonwv@gmail.com>
2020-01-25 12:29:19 -0500

    clone different templates with -t

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
   name = "easy-heroku-flask",
-  version = "0.0.2",
+  version = "0.0.3",
   author = "Casey Johnson",
   author_email = "caseyjohnsonwv@gmail.com",
   description = "Create new Heroku/Flask file structures in one command.",
